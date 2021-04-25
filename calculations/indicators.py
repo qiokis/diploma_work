983c79d66bbcdc360ccc56847d2c86c84f20fd63
qiokis <70791542+qiokis@users.noreply.github.com>
2021-04-25 16:27:03 +0800

    documentation

--- calculations/indicators.py
+++ calculations/indicators.py
@@ -14,12 +14,29 @@ class Indicators:
                                   "Delta": None}}
 
     def set_statistical(self, math_exp, disp, stand_dev, delta):
+        """
+        Method save values to dict
+        :param math_exp: math expectation
+        :param disp: dispersion
+        :param stand_dev: standard deviation
+        :param delta: delta
+        :return:
+        """
         self.indicators["Statistical"]["Math expectation"] = math_exp
         self.indicators["Statistical"]["Dispersion"] = disp
         self.indicators["Statistical"]["Standard Deviation"] = stand_dev
         self.indicators["Statistical"]["Delta"] = delta
 
     def set_analytical(self, math_exp, disp, stand_dev, var_coef, gamma):
+        """
+        Method save values to dict
+        :param math_exp: math expectation
+        :param disp: dispersion
+        :param stand_dev:standard deviation
+        :param var_coef: variation coefficient
+        :param gamma: x gamma
+        :return:
+        """
         self.indicators["Analytical"]["Math expectation"] = math_exp
         self.indicators["Analytical"]["Dispersion"] = disp
         self.indicators["Analytical"]["Standard Deviation"] = stand_dev
@@ -27,9 +44,17 @@ class Indicators:
         self.indicators["Analytical"]["X gamma"] = gamma
 
     def __str__(self):
+        """
+        Method present a string format of indicators
+        :return:
+        """
         return self.indicators.__str__()
 
     def clear(self):
+        """
+        Method clears indicators dict
+        :return:
+        """
         self.indicators = {"Analytical": {"Math expectation": None,
                                           "Dispersion": None,
                                           "Standard Deviation": None,
