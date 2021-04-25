8f6d49a9d8e67ac261432dbf676f2b5b8a4fe7bf
qiokis <70791542+qiokis@users.noreply.github.com>
2021-04-25 14:52:28 +0800

    fixed dictionary

--- calculations/indicators.py
+++ calculations/indicators.py
@@ -14,14 +14,28 @@ class Indicators:
                                   "Delta": None}}
 
     def set_statistical(self, math_exp, disp, stand_dev, delta):
-        self.indicators["Math expectation"] = math_exp
-        self.indicators["Dispersion"] = disp
-        self.indicators["Standard Deviation"] = stand_dev
-        self.indicators["Delta"] = delta
+        self.indicators["Statistical"]["Math expectation"] = math_exp
+        self.indicators["Statistical"]["Dispersion"] = disp
+        self.indicators["Statistical"]["Standard Deviation"] = stand_dev
+        self.indicators["Statistical"]["Delta"] = delta
 
     def set_analytical(self, math_exp, disp, stand_dev, var_coef, gamma):
-        self.indicators["Math expectation"] = math_exp
-        self.indicators["Dispersion"] = disp
-        self.indicators["Standard Deviation"] = stand_dev
-        self.indicators["Variation coefficient"] = var_coef
-        self.indicators["X gamma"] = gamma
+        self.indicators["Analytical"]["Math expectation"] = math_exp
+        self.indicators["Analytical"]["Dispersion"] = disp
+        self.indicators["Analytical"]["Standard Deviation"] = stand_dev
+        self.indicators["Analytical"]["Variation coefficient"] = var_coef
+        self.indicators["Analytical"]["X gamma"] = gamma
+
+    def __str__(self):
+        return self.indicators.__str__()
+
+    def clear(self):
+        self.indicators = {"Analytical": {"Math expectation": None,
+                                          "Dispersion": None,
+                                          "Standard Deviation": None,
+                                          "Variation coefficient": None,
+                                          "X gamma": None},
+                           "Statistical": {"Math expectation": None,
+                                           "Dispersion": None,
+                                           "Standard Deviation": None,
+                                           "Delta": None}}
