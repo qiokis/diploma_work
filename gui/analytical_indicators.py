ada1180073fdfba63eaebce616d2716b9a87e81b
qiokis <70791542+qiokis@users.noreply.github.com>
2021-04-25 16:26:06 +0800

    set_data func, documentation

--- gui/analytical_indicators.py
+++ gui/analytical_indicators.py
@@ -2,6 +2,9 @@ import tkinter as tk
 
 
 class AnalyticalIndicators(tk.LabelFrame):
+    """
+    Frame for analytical indicators
+    """
 
     width = 20
 
@@ -21,6 +24,20 @@ class AnalyticalIndicators(tk.LabelFrame):
         self.stand_dev.grid(row=11, column=0, padx=5, pady=5)
         self.var_coef = tk.Label(self, width=self.width, relief="sunken", bg="white", bd=1)
         self.var_coef.grid(row=11, column=1, padx=5, pady=5)
-        tk.Label(self, text="Standard deviation", width=self.width).grid(row=12, column=0, padx=5, pady=5)
+        tk.Label(self, text="X gamma", width=self.width).grid(row=12, column=0, padx=5, pady=5)
         self.gamma = tk.Label(self, width=self.width, relief="sunken", bg="white", bd=1)
         self.gamma.grid(row=13, column=0, padx=5, pady=5)
+
+    def set_data(self, values: dict):
+        """
+        Method records data to frame
+        :param values: data from calculator
+        :return:
+        """
+        self.math_exp["text"] = values["Analytical"]["Math expectation"]
+        self.dispersion["text"] = values["Analytical"]["Dispersion"]
+        self.stand_dev["text"] = values["Analytical"]["Standard Deviation"]
+        self.var_coef["text"] = values["Analytical"]["Variation coefficient"]
+        self.gamma["text"] = values["Analytical"]["X gamma"]
+
+
