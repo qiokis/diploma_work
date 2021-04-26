8d15535998dbd0e95658607253e46c4296b99331
qiokis <70791542+qiokis@users.noreply.github.com>
2021-04-26 18:13:47 +0800

    now just a frame for inputting data

--- gui/input_frame.py
+++ gui/input_frame.py
@@ -1,50 +1,26 @@
 import tkinter as tk
+from gui.widgets.input_widget import InputFrame
 
 
-class InputFrame(tk.LabelFrame):
+class MainFrame(tk.Frame):
     """
-    Frame with entry fields for input data
+    Main frame which combines all functional frames and gives acces to them
     """
 
-    width = 20
-
-    def __init__(self, root, c_spec=True):
+    def __init__(self, root):
         super().__init__(root)
-        self.configure(text="Input")
-        self.c_spec = c_spec
 
-        tk.Label(self, text="Indicator a", width=self.width).grid(row=0, column=0, padx=5, pady=5)
-        tk.Label(self, text="Indicator b", width=self.width).grid(row=0, column=1, padx=5, pady=5)
-        self.a_field = tk.Entry(self, width=self.width)
-        self.a_field.grid(row=1, column=0, padx=5, pady=5)
-        self.b_field = tk.Entry(self, width=self.width)
-        self.b_field.grid(row=1, column=1, padx=5, pady=5)
-        if self.c_spec:
-            tk.Label(self, text="Indicator c", width=self.width).grid(row=4, column=0, padx=5, pady=5)
-            self.c_field = tk.Entry(self, width=self.width)
-            self.c_field.grid(row=5, column=0, padx=5, pady=5)
-        else:
-            tk.Label(self, text="Indicator c_avg", width=self.width).grid(row=4, column=0, padx=5, pady=5)
-            self.c_avg_field = tk.Entry(self, width=self.width)
-            self.c_avg_field.grid(row=5, column=0, padx=5, pady=5)
-            tk.Label(self, text="Indicator kc", width=self.width).grid(row=4, column=1, padx=5, pady=5)
-            self.kc_field = tk.Entry(self, width=self.width)
-            self.kc_field.grid(row=5, column=1, padx=5, pady=5)
+        self.c_spec_frame = tk.LabelFrame(self, text="c is specified")
+        self.c_spec_frame.pack(padx=10, pady=10)
+
+        self.input_left = InputFrame(self.c_spec_frame)
+        self.input_left.pack(padx=10, pady=10)
+
+        self.c_not_spec_frame = tk.LabelFrame(self, text="c is not specified")
+        self.c_not_spec_frame.pack(padx=10, pady=10)
+
+        self.input_right = InputFrame(self.c_not_spec_frame, False)
+        self.input_right.pack(padx=10, pady=10)
 
         self.confirm = tk.Button(self, text="Calculate")
-        self.confirm.grid(row=6, column=0, columnspan=3, padx=5, pady=5)
-
-    def get_data(self):
-        """
-        Method returns data from fields
-        :return: data
-        """
-        if self.c_spec:
-            return {"a": self.a_field.get(),
-                    "b": self.b_field.get(),
-                    "c": self.c_field.get()}
-        else:
-            return {"a": self.a_field.get(),
-                    "b": self.b_field.get(),
-                    "c_avg": self.c_avg_field.get(),
-                    "kc": self.kc_field.get()}
+        self.confirm.pack(padx=5, pady=5)
