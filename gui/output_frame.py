af6064633e7e5340b3980cca66118b5f215a3ede
qiokis <70791542+qiokis@users.noreply.github.com>
2021-04-26 19:26:54 +0800

    a bit of fixes

--- gui/output_frame.py
+++ gui/output_frame.py
@@ -8,8 +8,11 @@ class OutputFrame(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
 
+        self.main_frame = tk.Frame(self)
+        self.main_frame.pack(side=tk.TOP)
+
         # c is specified
-        self.c_spec_frame = tk.LabelFrame(self, text="c is specified")
+        self.c_spec_frame = tk.LabelFrame(self.main_frame, text="c is specified")
         self.c_spec_frame.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
         self.left_stat_ind = StatisticalIndicators(self.c_spec_frame)
         self.left_stat_ind.pack(padx=10, pady=10)
@@ -17,8 +20,11 @@ class OutputFrame(tk.Frame):
         self.analyt_ind.pack(padx=10, pady=10)
 
         # c is not specified
-        self.c_not_spec_frame = tk.LabelFrame(self, text="c is not specified")
+        self.c_not_spec_frame = tk.LabelFrame(self.main_frame, text="c is not specified")
         self.c_not_spec_frame.pack(padx=10, pady=10, fill=tk.Y, side=tk.RIGHT)
         self.right_stat_ind = StatisticalIndicators(self.c_not_spec_frame)
         self.right_stat_ind.pack(padx=10, pady=10)
+
+        self.to_input = tk.Button(self, text="Back to input")
+        self.to_input.pack(padx=5, pady=5, side=tk.BOTTOM)
 
