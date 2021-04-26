62eccc7cf51123f60b0f3ce04e53391beb6133ce
qiokis <70791542+qiokis@users.noreply.github.com>
2021-04-26 18:56:23 +0800

    now input frame

--- gui/input_frame.py
+++ gui/input_frame.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from gui.widgets.input_widget import InputFrame
+from gui.widgets.input_widget import InputWidget
 
 
-class MainFrame(tk.Frame):
+class InputFrame(tk.Frame):
     """
     Main frame which combines all functional frames and gives acces to them
     """
@@ -10,16 +10,16 @@ class MainFrame(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
 
+        # c is specified
         self.c_spec_frame = tk.LabelFrame(self, text="c is specified")
         self.c_spec_frame.pack(padx=10, pady=10)
-
-        self.input_left = InputFrame(self.c_spec_frame)
+        self.input_left = InputWidget(self.c_spec_frame)
         self.input_left.pack(padx=10, pady=10)
 
+        # c is not specified
         self.c_not_spec_frame = tk.LabelFrame(self, text="c is not specified")
         self.c_not_spec_frame.pack(padx=10, pady=10)
-
-        self.input_right = InputFrame(self.c_not_spec_frame, False)
+        self.input_right = InputWidget(self.c_not_spec_frame, False)
         self.input_right.pack(padx=10, pady=10)
 
         self.confirm = tk.Button(self, text="Calculate")
