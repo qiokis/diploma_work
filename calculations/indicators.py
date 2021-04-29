6ef9a9deab3bd12968a30fca563750b5bf8c4c56
qiokis <70791542+qiokis@users.noreply.github.com>
2021-04-29 16:22:48 +0800

    xr and xr_gamma added

--- calculations/indicators.py
+++ calculations/indicators.py
@@ -11,11 +11,16 @@ class Indicators:
                   "Statistical": {"Math expectation": None,
                                   "Dispersion": None,
                                   "Standard Deviation": None,
-                                  "Conf int": None}}
+                                  "Conf int": None,
+                                  "Xr": None,
+                                  "Xr gamma": None}
+                  }
 
     def set_statistical(self, math_exp, disp, stand_dev, conf_int):
         """
         Method save values to dict
+        :param xr_gamma: xr gamma
+        :param xr: xr
         :param math_exp: math expectation
         :param disp: dispersion
         :param stand_dev: standard deviation
@@ -26,6 +31,10 @@ class Indicators:
         self.indicators["Statistical"]["Dispersion"] = disp
         self.indicators["Statistical"]["Standard Deviation"] = stand_dev
         self.indicators["Statistical"]["Conf int"] = conf_int
+
+    def set_xrs(self, values):
+        self.indicators["Statistical"]["Xr"] = values["xr"]
+        self.indicators["Statistical"]["Xr gamma"] = values["xr_gamma"]
 
     def set_analytical(self, math_exp, disp, stand_dev, var_coef, gamma):
         """
