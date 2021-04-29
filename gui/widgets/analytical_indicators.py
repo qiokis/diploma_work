3011c09a7acf50bb036069f8f84a468a6725a316
qiokis <70791542+qiokis@users.noreply.github.com>
2021-04-29 16:45:54 +0800

    ru

--- gui/widgets/analytical_indicators.py
+++ gui/widgets/analytical_indicators.py
@@ -11,21 +11,21 @@ class AnalyticalIndicators(tk.LabelFrame):
 
     def __init__(self, root):
         super().__init__(root)
-        self.configure(text="Analytical")
+        self.configure(text="Аналитика")
 
-        tk.Label(self, text="Math expectation", width=self.width).grid(row=8, column=0, padx=5, pady=5)
-        tk.Label(self, text="Dispersion", width=self.width).grid(row=8, column=1, padx=5, pady=5)
+        tk.Label(self, text="Мат. ожидание", width=self.width).grid(row=8, column=0, padx=5, pady=5)
+        tk.Label(self, text="Дисперсия", width=self.width).grid(row=8, column=1, padx=5, pady=5)
         self.math_exp = tk.Label(self, width=self.width, relief="sunken", bg="white", bd=1)
         self.math_exp.grid(row=9, column=0, padx=5, pady=5)
         self.dispersion = tk.Label(self, width=self.width, relief="sunken", bg="white", bd=1)
         self.dispersion.grid(row=9, column=1, padx=5, pady=5)
-        tk.Label(self, text="Standard deviation", width=self.width).grid(row=10, column=0, padx=5, pady=5)
-        tk.Label(self, text="Variation coefficient", width=self.width).grid(row=10, column=1, padx=5, pady=5)
+        tk.Label(self, text="Станд. отклонение", width=self.width).grid(row=10, column=0, padx=5, pady=5)
+        tk.Label(self, text="Коэф. вариации", width=self.width).grid(row=10, column=1, padx=5, pady=5)
         self.stand_dev = tk.Label(self, width=self.width, relief="sunken", bg="white", bd=1)
         self.stand_dev.grid(row=11, column=0, padx=5, pady=5)
         self.var_coef = tk.Label(self, width=self.width, relief="sunken", bg="white", bd=1)
         self.var_coef.grid(row=11, column=1, padx=5, pady=5)
-        tk.Label(self, text="X gamma", width=self.width).grid(row=12, column=0, padx=5, pady=5)
+        tk.Label(self, text="X гамма", width=self.width).grid(row=12, column=0, padx=5, pady=5)
         self.gamma = tk.Label(self, width=self.width, relief="sunken", bg="white", bd=1)
         self.gamma.grid(row=13, column=0, padx=5, pady=5)
 
