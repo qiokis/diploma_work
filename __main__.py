59d1d8f8656dc6ad4fd41b8e272c73ad36fb946c
qiokis <70791542+qiokis@users.noreply.github.com>
2021-05-05 15:22:40 +0800

    controller renamed to controllers

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ __author__ = "qiokis"
 
 if __name__ == '__main__':
     import tkinter as tk
-    from controller.main_controller import MainController
+    from controllers.main_controller import MainController
 
     root = tk.Tk()
     MainController(root)
